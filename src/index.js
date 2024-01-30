f9a8617406f76ebf131462ad1ccf0e5166e96a8a
Sally Do <56125850+sallydough@users.noreply.github.com>
2024-01-29 21:23:51 -0700

    remove pwd attempt

--- src/index.js
+++ src/index.js
@@ -5,17 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { StyledEngineProvider } from '@mui/material/styles';
 import Modal from "react-modal";
-import * as serviceWorker from './sw';
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js')
-    .then((registration) => {
-      console.log('Service Worker registered with scope:', registration.scope);
-    })
-    .catch((error) => {
-      console.error('Service Worker registration failed:', error);
-    });
-}
 
 Modal.setAppElement('#root'); // Check this line, ensure it matches the selector used in ReactDOM.createRoot
 
@@ -33,7 +23,4 @@ root.render(
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+
